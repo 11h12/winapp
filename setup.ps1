09c19fb3f601ea80708ab9eef40c92be2bfe9105
duong <46297204+11h12@users.noreply.github.com>
2026-02-25 07:37:30 -0800

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -7,7 +7,7 @@ $MyApps = @(
         Version    = "1.0.5"
         LastUpdate = "2025-01-19"
         InstallCmd = "/S"
-        Url        = "https://nas.dpham.one/sharing/pfuxuyFVG?download=true"
+        Url        = "https://nas.dpham.one/sharing/93elgvc4u?download=true"
     },
     [PSCustomObject]@{
         Name       = "Daily App (Mini Trello)"
